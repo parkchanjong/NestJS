71a78d9c9ca5dc5d5284e9679f18878962338edc
parkchanjong <7000cj@gmail.com>
2021-05-17 01:38:36 +0900

    user 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController, MoviesController],
   providers: [AppService, MoviesService],
 })
