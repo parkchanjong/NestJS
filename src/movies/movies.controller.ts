b2e41df00dac1ccb3bb7e974ffa9f72fb6062500
parkchanjong <7000cj@gmail.com>
2021-05-20 16:58:00 +0900

    [movies] movies service 로직수정
    - movies service 로직을 수정한다.

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -28,9 +28,6 @@ export class MoviesController {
 
   @Patch(':id')
   patch(@Param('id') movieId: string, @Body() updateData) {
-    return {
-      updateMovie: movieId,
-      ...updateData,
-    };
+    return this.moviesService.update(movieId, updateData);
   }
 }
